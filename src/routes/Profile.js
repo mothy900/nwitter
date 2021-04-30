fc51b50d17a0cf7c823f0ecc5acebbd43af13fdc
mothy900 <mothy900@naver.com>
2021-04-30 20:16:41 +0900

    profile picture upload complete

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -8,6 +8,7 @@ export default ({ refreshUser, userObj }) => {
   const history = useHistory();
   const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const [profilePic, setProfilePic] = useState("");
+  const [picture, setPicture] = useState(null);
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
@@ -41,25 +42,131 @@ export default ({ refreshUser, userObj }) => {
         .child(`profilePic/${userObj.uid}`);
       const respons = await profilePicREf.putString(profilePic, "data_url");
       profilePicUrl = await respons.ref.getDownloadURL();
-    }
-    const profileObj = {
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-      profilePicUrl,
-    };
-    // 저장한 뒤엔 원래 있던 사진을 삭제해야함.
 
-    try {
-      await dbService.collection("profilePic").doc(`*`).delete(); // 기존에 있던 프로필 사진 삭제
-      //console.log();
-      await dbService.collection("profilePic").add(profileObj); //db collection 없으면 생성
-      setProfilePic("");
-    } catch (error) {
-      console.log(error);
+      const profileObj = {
+        createdAt: Date.now(),
+        creatorId: userObj.uid,
+        profilePicUrl,
+      };
+
+      // 저장한 뒤엔 원래 있던 사진을 삭제해야함.
+
+      try {
+        await dbService
+          .collection("profilePic")
+          .doc(`IRQoXujxtcv93a8yILOQ`)
+          .delete(); // 기존에 있던 프로필 사진 삭제
+        //console.log();
+        await dbService
+          .collection("profilePic")
+          .doc(`${userObj.uid}`)
+          .set(profileObj); //db collection 없으면 생성
+        setProfilePic("");
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
-  ////////////////////////////////////////////////////
-  /*async function deleteCollection(db, collectionPath, batchSize) {
+  const watchProfilePic = async () => {
+    const changePicture = dbService
+      .collection("profilePic")
+      .doc(`${userObj.uid}`);
+
+    const doc = await changePicture.get();
+    if (!doc.exists) {
+      console.log("No such document!");
+    } else {
+      console.log("Document data:", doc.data());
+    }
+    setPicture(doc.data().profilePicUrl);
+  };
+  useEffect(() => {
+    //변화가 감지될 때 마다 실행
+    getMyNweets();
+    watchProfilePic();
+  });
+
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setProfilePic(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+  const onClearAttachment = () => setProfilePic("");
+  return (
+    <div className="container">
+      <form onSubmit={onSubmit} className="profileForm">
+        <div className="profile_Img">
+          <img src={`${picture}`}></img>
+        </div>
+        <input
+          onChange={onChange}
+          type="text"
+          autoFocus
+          placeholder="Display name"
+          value={newDisplayName}
+          className="formInput"
+        />
+        <input
+          type="submit"
+          value="Update Profile"
+          className="formBtn"
+          style={{
+            marginTop: 10,
+          }}
+          //onClick={onClearAttachment}
+          // submit 하면 프로파일의 사진 미리보기 삭제
+        />
+        <div className="profile-pic-upload">
+          <label htmlFor="profile-pic" className="profilePic_input">
+            <span>Add photos</span>
+            <FontAwesomeIcon icon={faPlus} />
+          </label>
+        </div>
+        <input
+          id="profile-pic"
+          type="file"
+          accept="image/*"
+          onChange={onFileChange}
+          style={{
+            opacity: 0,
+            height: 0,
+          }}
+        />
+        {/** preview */}
+        {profilePic && (
+          <div className="factoryForm__profilePic">
+            <img
+              src={`${profilePic}`}
+              style={{
+                backgroundImage: profilePic,
+              }}
+            />
+            <div className="factoryForm__clear" onClick={onClearAttachment}>
+              <span>Remove</span>
+              <FontAwesomeIcon icon={faTimes} />
+            </div>
+          </div>
+        )}
+      </form>
+      <span className="formBtn cancelBtn logOut" onClick={onLogOutClick}>
+        Log Out
+      </span>
+    </div>
+  );
+};
+
+////////////////////////////////////////////////////
+//collection 자체를 삭제하는 코드 -> collection 내부 요소를 모두 삭제한 뒤 진행한다.
+/*async function deleteCollection(db, collectionPath, batchSize) {
     const collectionRef = db.collection(collectionPath);
     const query = collectionRef.orderBy('__name__').limit(batchSize);
   
@@ -91,83 +198,4 @@ export default ({ refreshUser, userObj }) => {
       deleteQueryBatch(db, query, resolve);
     });
   }*/
-  ////////////////////////////////////////////////////
-  useEffect(() => {
-    //변화가 감지될 때 마다 실행
-    getMyNweets();
-  });
-
-  const onFileChange = (event) => {
-    const {
-      target: { files },
-    } = event;
-    const theFile = files[0];
-    const reader = new FileReader();
-    reader.onloadend = (finishedEvent) => {
-      const {
-        currentTarget: { result },
-      } = finishedEvent;
-      setProfilePic(result);
-    };
-    reader.readAsDataURL(theFile);
-  };
-  const onClearAttachment = () => setProfilePic("");
-  return (
-    <div className="container">
-      <form onSubmit={onSubmit} className="profileForm">
-        <div className="profile_Img">
-          <img src={"22"}></img>
-        </div>
-        <input
-          onChange={onChange}
-          type="text"
-          autoFocus
-          placeholder="Display name"
-          value={newDisplayName}
-          className="formInput"
-        />
-        <input
-          type="submit"
-          value="Update Profile"
-          className="formBtn"
-          style={{
-            marginTop: 10,
-          }}
-          //onClick={onClearAttachment}
-          // submit 하면 프로파일의 사진 미리보기 삭제
-        />
-        <label htmlFor="profile-pic" className="profilePic_input">
-          <span>Add photos</span>
-          <FontAwesomeIcon icon={faPlus} />
-        </label>
-        <input
-          id="profile-pic"
-          type="file"
-          accept="image/*"
-          onChange={onFileChange}
-          style={{
-            opacity: 100,
-          }}
-        />
-        {/** preview */}
-        {profilePic && (
-          <div className="factoryForm__profilePic">
-            <img
-              src={`${profilePic}`}
-              style={{
-                backgroundImage: profilePic,
-              }}
-            />
-            <div className="factoryForm__clear" onClick={onClearAttachment}>
-              <span>Remove</span>
-              <FontAwesomeIcon icon={faTimes} />
-            </div>
-          </div>
-        )}
-      </form>
-      <span className="formBtn cancelBtn logOut" onClick={onLogOutClick}>
-        Log Out
-      </span>
-    </div>
-  );
-};
+////////////////////////////////////////////////////
